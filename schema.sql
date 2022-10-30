9dcb15f8441558a4ebe08ad06e515c002f46ca16
Mparra14 <107594924+Mparra14@users.noreply.github.com>
2022-10-30 11:11:14 -0400

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -1,20 +1,31 @@
-CREATE TABLE Country_of_Origin (
-	variety varchar ,
-	country varchar ,
-	points int,
+
+-- Original csv file database table
+CREATE TABLE Wine_DB (
+	country varchar (150000) NOT NULL ,
+	description text NOT NULL,
+	designation varchar,
+	points int NOT NULL,
+	price int NOT NULL,
+	province varchar,
+	region_1 varchar,
+	region_2 varchar, 
+	variety varchar NOT NULL,
+	winery varchar,
 	PRIMARY KEY (variety)
+
 );
 
-CREATE TABLE Wine_Price (
-	variety varchar,
-	price int,
-	points int,
-	PRIMARY KEY (variety)
-);
+-- create  new table with only country, points and variety 
+SELECT country, points, variety
+FROM Wine_DB
+INTO Country_of_Origin ;
 
-CREATE TABLE Wine_Description (
-	variety varchar,
-	description varchar,
-	points int,
-	PRIMARY KEY (variety)
-);
+-- Create new table with only price, points, and variety
+SELECT price, points, variety
+FROM Wine_DB
+INTO Wine_Price ;
+
+-- Create new table with only description, points, and variety
+SELECT description, points, variety
+FROM Wine_DB
+INTO Country_of_Origin ;
